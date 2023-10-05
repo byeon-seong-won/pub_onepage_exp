f5307812be659259d849e5e7393f86994eb6bc03
Seongwon <sngoneee@gmail.com>
2023-10-05 15:22:12 +0900

    edg 수정

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
+ 
 
 
-    $('#wrap .container .cont03 form fieldset .bottom>div').click(function() {
+    $('#wrap .container .cont03 div .fieldset .bottom>div').click(function() {
       $(this).find(".xi-check").toggleClass("active");
     });
 
@@ -16,6 +17,33 @@ $(document).ready(function() {
             scrollTop: $("#submit").offset().top
         });
     });
+
+
+    // 브라우저 resize 처리
+    window.addEventListener("resize", function() {
+        var innerWidth = window.innerWidth;
+        if(innerWidth >= 1280) { 
+          $(window).scrollTop(0);
+        } 
+    })
+
+
+
+    // 보기 클릭시 모달창 (열기)
+    $(".view").click(function() {
+        $(this).parent().find(".topBg").css({"display":"block"})
+        $(this).parent().find(".topPp").css({"display":"block"})
+        $("body").css({"overflow" : "hidden"})
+    })
+
+    // (닫기)
+    $(".close").click(function() {
+      $(".topBg").css({"display":"none"})
+      $(".topPp").css({"display":"none"})
+      $("body").css({"overflow":"visible"})
+    })
+      
+  
 
 
 
